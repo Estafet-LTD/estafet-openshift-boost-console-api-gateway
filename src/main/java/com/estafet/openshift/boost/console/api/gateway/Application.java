192e8d25443ab9ff7719ef75edf665081f8f3e4c
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-10 18:16:37 +0000

    enabling cross browser

--- src/main/java/com/estafet/openshift/boost/console/api/gateway/Application.java
+++ src/main/java/com/estafet/openshift/boost/console/api/gateway/Application.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan
@@ -30,6 +33,19 @@ public class Application extends SpringBootServletInitializer {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
 		return restTemplateBuilder.build();
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Headers")
+						.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Headers");
+			}
+		};
 	}
 	
 }

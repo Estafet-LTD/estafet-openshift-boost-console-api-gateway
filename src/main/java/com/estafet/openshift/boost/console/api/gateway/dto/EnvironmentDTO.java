3488b0a3b1047c271ea88ea6d2a7a042ac0ddcfc
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-10 15:22:46 +0000

    import the json include annotation

--- src/main/java/com/estafet/openshift/boost/console/api/gateway/dto/EnvironmentDTO.java
+++ src/main/java/com/estafet/openshift/boost/console/api/gateway/dto/EnvironmentDTO.java
@@ -3,6 +3,9 @@ package com.estafet.openshift.boost.console.api.gateway.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 @JsonInclude(Include.NON_NULL)
 public class EnvironmentDTO {
 

0a9a6f7ee4407c6fe19eebdd26c5315d8ec664e0
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-24 11:38:50 +0000

    The environment variable name was incorrect. Now fixed

--- src/main/java/com/estafet/openshift/boost/console/api/gateway/util/ENV.java
+++ src/main/java/com/estafet/openshift/boost/console/api/gateway/util/ENV.java
@@ -4,7 +4,7 @@ public final class ENV {
 
 	private ENV( ) {}
 		
-	public static final String ENVIRONMENT_SERVICE_API = System.getenv("ENVIRONMENT_SERVICE_API_URI");
+	public static final String ENVIRONMENT_SERVICE_API = System.getenv("ENVIRONMENT_API_SERVICE_URI");
 	public static final String FEATURE_SERVICE_API = System.getenv("FEATURE_API_SERVICE_URI");
 	public static final String JENKINS_SERVICE_API = System.getenv("JENKINS_API_SERVICE_URI");
 	public static final String USER_SERVICE_API = System.getenv("USER_API_SERVICE_URI");

bfd8379b5c2c2f4138a647fd527c0cb5a3a2bf71
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-04-14 21:54:12 +0100

    proxy webhooks to the environment ms

---
+++ src/main/java/com/estafet/openshift/boost/console/api/gateway/service/GitHubService.java
@@ -0,0 +1,21 @@
+package com.estafet.openshift.boost.console.api.gateway.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.estafet.openshift.boost.console.api.gateway.util.ENV;
+import com.estafet.openshift.boost.messages.github.GitHubHook;
+
+@Service
+public class GitHubService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public String webhook(GitHubHook hook) {
+		return restTemplate.postForObject(ENV.ENVIRONMENT_SERVICE_API + 
+				"/hooks", hook, String.class);
+	}
+
+}
